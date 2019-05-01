d5e338f3c8dc86fbe4611426a35f48e22108d8e9
idantp <idantp@gmail.com>
2019-05-01 20:00:46 +0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,8 @@
+// Author: Idan Twito
+// ID:     311125249
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ threadPool.c
@@ -0,0 +1,9 @@
+// Author: Idan Twito
+// ID:     311125249
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
